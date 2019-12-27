d44ee5fcd62a16483555bf39654e17fee24347c2
Fellipe Augusto Ugliara <ugliara.fellipe@gmail.com>
2019-12-26 22:53:15 -0300

    script: Implement builder of the project and format code files

---
+++ build.sh
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2019, Fellipe Augusto Ugliara
+#
+# Use of this source code is governed by an ISC license that can be found 
+# in the LICENSE file at https://github.com/ugliara-fellipe/weblog.staticgen
+#
+# --------------------
+#
+# Run this script to deploy extend library and tests
+# It require clang, and ar; to install use the command:
+#
+#   $ sudo apt install clang
+#
+rm -rf ./deploy
+mkdir deploy
+mkdir deploy/object
+
+EXTEND="../library.extend/deploy"
+MODULE_INC="-I $EXTEND/include"
+EXTEND_INC="$MODULE_INC/extend $MODULE_INC/regex $MODULE_INC/recognizer"
+EXTEND_LIB="-L $EXTEND/lib/ -l extend"
+
+clang compiler/home.c -c $EXTEND_INC -o deploy/object/home.o
+clang compiler/post.c -c $EXTEND_INC -o deploy/object/post.o
+clang compiler/site.c -c $EXTEND_INC -o deploy/object/site.o
+
+clang cli.c deploy/object/*.o -I compiler $EXTEND_INC $EXTEND_LIB -o deploy/staticgen
---
+++ format.sh
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2019, Fellipe Augusto Ugliara
+#
+# Use of this source code is governed by an ISC license that can be found 
+# in the LICENSE file at https://github.com/ugliara-fellipe/weblog.staticgen
+#
+# --------------------
+#
+# Run this script to formatter all the C code files
+# It require clang-format, to install use the command:
+#
+#   $ sudo apt install clang-format
+#
+clang-format -i *.c
+clang-format -i */*.h
+clang-format -i */*.c
